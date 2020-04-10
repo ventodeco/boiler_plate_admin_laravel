a7f3cddd340df9006abb2cd4246ca0e81ad9898d
ventodeco <ventodecoboagoes@gmail.com>
2020-04-10 20:21:56 +0700

    add files

---
+++ resources/views/admin/templates/partials/sidebar.blade.php
@@ -0,0 +1,16 @@
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+  
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+          
+
+        <li><a href=""><i class="fa fa-book"></i> <span>Buku</span></a></li>
+        
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
